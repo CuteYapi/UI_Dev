5a1aeee9bdb23259dd5262d932574fb6238e1c01
CuteYapi <yapi42@gmail.com>
2025-03-30 15:30:16 +0900

    First Commit

---
+++ Assets/100_Script/20_UI/DeckController.cs
@@ -0,0 +1,58 @@
+using UnityEngine;
+using System.Collections.Generic;
+using ClassDefine;
+
+public class DeckController : MonoBehaviour, IUI
+{
+    public ButtonController CloseButton;
+    
+    public ButtonController[] SkillUpButtonArray = new ButtonController[5];
+
+    public TextView[] TextViewArray = new TextView[9];
+
+    public void Initialize()
+    {
+        CloseButton.Initialize();
+        CloseButton.SetButtonAction(() =>
+        {
+            Close();
+        });
+
+        for (int i = 0; i < SkillUpButtonArray.Length; i++)
+        {
+            SkillUpButtonArray[i].Initialize();
+            SkillUpButtonArray[i].SetButtonAction(() =>
+            {
+                UI.SkillUpController.Open();
+            });
+        }
+
+        Open();
+    }
+
+    public void Open()
+    {
+        gameObject.SetActive(true);
+        SetStudentDataText();
+    }
+
+    public void Close()
+    {
+        gameObject.SetActive(false);
+    }
+
+    private void SetStudentDataText()
+    {
+        StudentInfo currentStudent = Manager.Data.OwnStudentInfoList[0];
+
+        TextViewArray[0].SetText(currentStudent.Hp);
+        TextViewArray[1].SetText(currentStudent.AttackPower);
+        TextViewArray[2].SetText(currentStudent.Defense);
+        TextViewArray[3].SetText(currentStudent.HealAmount);
+        TextViewArray[4].SetText(currentStudent.Name);
+        TextViewArray[5].SetText(currentStudent.CombatRole.ToString());
+        TextViewArray[6].SetText(currentStudent.CombatPosition.ToString());
+        TextViewArray[7].SetText(currentStudent.AttackType.ToString());
+        TextViewArray[8].SetText(currentStudent.ArmorType.ToString());
+    }
+}
